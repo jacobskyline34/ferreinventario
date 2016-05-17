deb300680e828b0de096fa1b1ab26c9681fe271b
SaMoSe12 <santiago.moguel.secades@gmail.com>
2016-05-17 12:13:17 -0500

    Espero que funcione

--- proveedoreditado.php
+++ proveedoreditado.php
@@ -17,9 +17,8 @@ if (isset($_POST["modificar"])) {
 	$codigo=$_POST["codigo"];
 	$nombre=$_POST["nombre"];
 	$comentario=$_POST["comentario"];
-	$modificar="UPDATE tb_proveedores SET nombre='$nombre',Comentarios='$comentario' WHERE id_Proveedores='$codigo'";
-	
-	mysql_query($modificar);
+	$modificar="UPDATE tb_proveedores SET nombre='{$nombre}',Comentarios='{$comentario}' WHERE id_Proveedores='{$codigo}'", $res = mysql_query($modificar);
+    if(!$res){echo mysql_error()};
 	echo "<p>Proveedor modificado con éxito <a href='proovedores.php'>Volver</a></p>";
 } else {
 	echo "<p>Olvido pasar por la <a href='editarproveedor.php'>lista de proveedores</a></p>";
