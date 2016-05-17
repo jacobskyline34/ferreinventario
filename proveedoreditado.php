503729e839c8fd373638f3189b9db1bb5c53ccb5
SaMoSe12 <santiago.moguel.secades@gmail.com>
2016-05-17 12:53:19 -0500

    Timer

--- proveedoreditado.php
+++ proveedoreditado.php
@@ -17,12 +17,15 @@ if (isset($_POST["modificar"])) {
 	$codigo=$_POST["codigo"];
 	$nombre=$_POST["nombre"];
 	$comentario=$_POST["comentario"];
-	$modificar="UPDATE tb_proveedores SET nombre='{$nombre}',Comentarios='{$comentario}' WHERE id_Proveedores='{$codigo}'", $res = mysql_query($modificar);
+	$modificar="UPDATE tb_proveedores SET nombre='{$nombre}', Comentarios='{$comentario}', WHERE id_Proveedores='{$codigo}'", $res= mysql_query($modificar);
     if(!$res){echo mysql_error()};
-	echo "<p>Proveedor modificado con éxito <a href='proovedores.php'>Volver</a></p>";
+	/* echo "<p>Proveedor modificado con éxito <a href='proovedores.php'>Volver</a></p>";
 } else {
-	echo "<p>Olvido pasar por la <a href='editarproveedor.php'>lista de proveedores</a></p>";
+	echo "<p>Olvido pasar por la <a href='editarproveedor.php'>lista de proveedores</a></p>";*/
 }
 ?>
+    <script type="text/javascript">
+            setTimeout("location.href = 'provedores.php';", 200);
+    </script>
 </body>
 </html>
